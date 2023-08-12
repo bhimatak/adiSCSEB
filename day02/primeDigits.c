cf0c06b1b99a049de77276ac0ee6764f5123d68a
bhimatak <bhima.t@gmail.com>
2023-08-12 10:06:38 +0530

    updated

--- day02/primeDigits.c
+++ day02/primeDigits.c
@@ -1,11 +1,40 @@
+#include <stdio.h>
+
 int isPrime(int);
 int getDigits(int);
 
+
+
+int main()
+{
+
+	int i, n, temp;
+	int countN = 0;
+	scanf("%d",&n);
+
+	for(i=0;;i++)
+	{
+		/*if (countN == n)
+			break;
+			*/
+
+		if (getDigits(i)==1)
+		{
+			countN++;
+			if(countN == n)
+				break;
+		}
+	}
+
+	printf("nth Number: %d",i);
+
+}
 
 int getDigits(int num)
 {
 	int countDigits = 0;
 	int countPFlag = 0;
+	int r;
 	while(num>0)
 	{
 		r = num%10;
@@ -17,7 +46,7 @@ int getDigits(int num)
 		num /= 10;
 	}
 
-	if (countPFlag == countDigits)
+	if ((countPFlag == countDigits)&&(countPFlag!=0))
 		return 1;
 	else
 		return 0;
@@ -27,7 +56,7 @@ int isPrime(int num)
 {
 	int i, flag =0;
 
-	if(num <=0)
+	if(num <=1)
 		return 1;
 	for(i=2;i<=num/2;i++)
 	{
