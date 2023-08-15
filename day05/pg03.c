c0e0a6cc0eb607ad13fdb5ff5d559731ba42e644
bhimatak <bhima.t@gmail.com>
2023-08-15 11:55:53 +0530

    Updated

--- day05/pg03.c
+++ day05/pg03.c
@@ -20,6 +20,9 @@ int main()
 	
 	StoreElems(ptr,n);
 	dispElems(ptr,n);
+	printf("\nAfter Sorting\n");
+	sortElems(ptr,n);
+	dispElems(ptr,n);
 	printf("\n\n");
 	free(ptr);
 	return 0;	
@@ -43,4 +46,26 @@ void StoreElems(int *ptr, int n)
 		/* code */
 		scanf("%d",ptr);
 	}
+}
+
+void sortElems(int *ptr, int n)
+{
+	int temp, i,j;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(ptr[i]<ptr[j])
+			{
+				temp = *(ptr+i);
+				//temp = ptr[i];
+				*(ptr+i) = *(ptr+j);
+				// ptr[i] = ptr[j];
+				*(ptr+j) = temp;
+				// ptr[j] = temp;
+			}
+		}
+
+	}
+
 }
